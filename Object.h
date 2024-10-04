87cb8ef726950e6a91b01a50b20e5906a90c47ac
hyzboy <hyzboy@gmail.com>
2024-10-04 12:16:50 +0800

    maybe it's correcter SafePtr<>. TEST RUN OK!

--- Object.h
+++ Object.h
@@ -52,38 +52,73 @@ public: \
         return GetTypeHashCode<class_name>(); \
     }
 
-template<typename T> struct SafePtrData
-{
-    T *ptr;
-};
-
 /**
  * 安全访问指针<Br>
  * 其本质类似于的WeakPtr，但是不同的是:
  * <ul>
  *      <li>SafePtr不使用atom计数器，所以它不是线程安全的</li>
- *      <li>SafePtr不会自动释放指针，而是在访问时检查指针是否有效，如果无效则返回nullptr</li>
+ *      <li>SafePtr不会自动释放指针，它是在访问时检查指针是否有效，如果无效则返回nullptr</li>
  * </ul>
  */
 template<typename T> class SafePtr
 {
-    SafePtrData<T> data;
+    struct SafePtrData
+    {
+        T *ptr;
+        int count;
+
+    public:
+
+        SafePtrData(T *p)
+        {
+            ptr=p;
+            count=1;
+        }
+    };
+
+    SafePtrData *data;
+
+protected:
+
+    void unlink()
+    {
+        if(!data)
+            return;
+
+        if(data->count>0)
+            --data->count;
+
+        if(data->count==0)
+            delete data;
+
+        data=nullptr;
+    }
 
 public:
 
     SafePtr()
     {
-        data.ptr=nullptr;
+        data=nullptr;
     }
+
     SafePtr(T *ptr)
     {
-        data.ptr=ptr;
+        data=new SafePtrData(ptr);
     }
-    SafePtr(SafePtrData<T> &spd):data(spd){}
-    virtual ~SafePtr()=default;
 
-            T *Get()        {return data.ptr;}
-    const   T *Get() const  {return data.ptr;}
+    SafePtr(SafePtr<T> &sp)
+    {
+        data=nullptr;
+        operator=(sp);
+    }
+
+    virtual ~SafePtr()
+    {
+        unlink();
+    }
+
+            T *Get()        {return data?data->ptr:nullptr;}
+    const   T *Get() const  {return data?data->ptr:nullptr;}
 
     T *operator->() { return Get(); }
     T &operator*() { return *Get(); }
@@ -96,26 +131,44 @@ public:
 
     const bool IsValid() const noexcept
     {
-        return data.ptr;
+        return data&&data->ptr;
     }
 
-    SafePtr<T> &operator=(SafePtrData<T> &spd)
+    SafePtr<T> &operator=(SafePtr<T> &sp)
     {
-        data=spd;
+        if(data)
+        {
+            if(data->ptr==sp.ptr)
+                return *this;
+
+            unlink();
+        }
+
+        if(sp.IsValid())
+        {
+            data=sp.data;
+            ++data->count;
+        }
 
         return *this;
     }
 
     template<typename OT>
-    SafePtr<T> &operator=(SafePtrData<OT> &spd)
+    SafePtr<T> &operator=(SafePtr<OT> &spd)
     {
         if(T::StaticHashCode()!=OT.StaticHashCode())
         {
-            data.ptr=nullptr;
+            unlink();
             return *this;
         }
 
-        data.ptr=(T *)(spd.ptr);
+        if(data!=spd.data)
+        {
+            unlink();
+            data=spd.data;
+            ++data->count;
+        }
+
         return *this;
     }
 
@@ -123,26 +176,28 @@ public:
     {
         if(!obj)
         {
-            data.ptr=nullptr;
+            unlink();
             return *this;
         }
 
         if(T::StaticHashCode()!=obj->GetHashCode())
         {
-            data.ptr=nullptr;
+            unlink();
             return *this;
         }
 
-        data.ptr=obj;
+        data=new SafePtrData(obj);
         return *this;
     }
 
     void Destory()
     {
-        if(data.ptr)
+        if(data&&data->ptr)
         {
-            data.ptr->Destory();
-            data.ptr=nullptr;
+            data->ptr->Destory();
+            data->ptr=nullptr;
+
+            unlink();
         }
     }
 };//template<typename T> class SafePtr
